f9f3364fc8004a0513f5795a66da671daad8aa90
TheTripleV <vasistavovveti@gmail.com>
2020-08-24 19:50:40 -0500

    run black

--- tests/roots/builder/test-deleted_file_not_redirected_commit/conf.py
+++ tests/roots/builder/test-deleted_file_not_redirected_commit/conf.py
@@ -9,4 +9,7 @@ rediraffe_redirects = "redirects.txt"
 
 import subprocess
 from pathlib import Path
-rediraffe_branch = subprocess.check_output(f"git -C {Path(__file__).parent} rev-parse HEAD~1", shell=True).decode("utf-8")
+
+rediraffe_branch = subprocess.check_output(
+    f"git -C {Path(__file__).parent} rev-parse HEAD~1", shell=True
+).decode("utf-8")
