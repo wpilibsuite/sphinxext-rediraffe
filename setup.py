fe66b8e76c959fa51e968c872670b9a221765790
TheTripleV <vasistavovveti@gmail.com>
2021-06-11 04:57:36 -0500

    Match requirements structure to that of opengraph

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setuptools.setup(
     url="https://github.com/wpilibsuite/sphinxext-rediraffe",
     packages=["sphinxext"],
     install_requires=["sphinx>=2.0"],
-    extras_require={"rtd": ["myst-parser~=0.12.7", "sphinx_rtd_theme~=0.5.0"]},
     classifiers=[
         "Environment :: Plugins",
         "Environment :: Web Environment",
